741c22bb9c1241c5cc3657017d6925d7ab3f2d5e
Kunal Purswani <kunalpurswani40@gmail.com>
2023-11-28 03:14:14 +0530

    commit

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'Blogs Web',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'http://localhost:3000', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'kunalpurswani40@gmail.com', 
+    github: '/',
+    twitter: '/',
+    facebook: '/',
+    youtube: '/',
+    linkedin: '/',
+    dribbble: '/',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
